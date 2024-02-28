176bab38ce45a27315eeec73b7f858cf03789bbf
Orla Poole <orlapoole@gmail.com>
2024-02-28 21:07:12 +0000

    add nav bar, css and js hamburger function, and media query for mobile responsiveness

---
+++ assets/js/script.js
@@ -0,0 +1,13 @@
+// nav menu hamburger function 
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+
+hamburger.addEventListener("click", () => {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+})
+
+document.querySelectorAll(".nav-item").forEach(n => n.addEventListener("click", (event) => {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+}));
