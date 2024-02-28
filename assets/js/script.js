73944052adc8fddbd5a3925cc4b0cde3a2e333bb
Orla Poole <orlapoole@gmail.com>
2024-02-28 21:49:55 +0000

    add thank you styling and js function on contact form submission

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,3 +11,10 @@ document.querySelectorAll(".nav-item").forEach(n => n.addEventListener("click", 
     hamburger.classList.remove("active");
     navMenu.classList.remove("active");
 }));
+
+// bring users to thankyou.html when form submit button is pressed 
+document.getElementById("contact-form").addEventListener("submit", function(event) {
+    event.preventDefault();
+    
+    window.location.href = "thankyou.html";
+  });
