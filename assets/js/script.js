4bbc35b74d4634c835a95c0ce06393019b183dec
Orla Poole <orlapoole@gmail.com>
2024-02-29 20:59:02 +0000

    add js function for randomising dice results

--- assets/js/script.js
+++ assets/js/script.js
@@ -25,3 +25,14 @@ document.addEventListener("DOMContentLoaded", function() {
     }
 });
 
+// dice roll function
+let cube = document.getElementById('cube');
+
+angleArray = [[0,0,0],[-310,-362,-38],[-400,-320,-2],[135,-217,-88],[-224,-317,5],[-47,-219,-81],[-133,-360,-53]];
+
+cube.addEventListener('click',function(e){
+    const randomAngle = Math.floor(Math.random() * 6) +1;
+    //console.log(randomAngle);
+    cube.style.transform = 'rotateX('+angleArray[randomAngle][0]+'deg) rotateY('+angleArray[randomAngle][1]+'deg) rotateZ('+angleArray[randomAngle][2]+'deg)';
+});
+
