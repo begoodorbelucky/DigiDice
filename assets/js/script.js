eed284045e1a160fa1c18ad669f9163e0ec1336c
Orla Poole <orlapoole@gmail.com>
2024-02-29 21:13:44 +0000

    add dice roll function

--- assets/js/script.js
+++ assets/js/script.js
@@ -30,9 +30,11 @@ let cube = document.getElementById('cube');
 
 angleArray = [[0,0,0],[-310,-362,-38],[-400,-320,-2],[135,-217,-88],[-224,-317,5],[-47,-219,-81],[-133,-360,-53]];
 
-cube.addEventListener('click',function(e){
+cube.addEventListener('click',function(){
+
     const randomAngle = Math.floor(Math.random() * 6) +1;
     //console.log(randomAngle);
     cube.style.transform = 'rotateX('+angleArray[randomAngle][0]+'deg) rotateY('+angleArray[randomAngle][1]+'deg) rotateZ('+angleArray[randomAngle][2]+'deg)';
+    cube.style.transition = '1s linear'
 });
 
